252c95af55d1e81dbd394e248934789176ff1d19
Ark-Arch <69996990+Ark-Arch@users.noreply.github.com>
2025-04-16 12:32:48 +0000

    added foldeer structure and sample tasks

---
+++ src/assets/taskData.js
@@ -0,0 +1,47 @@
+export const tasks = [
+    {
+      id: 1,
+      name: 'Buy groceries',
+      description: 'Milk, eggs, bread',
+      dueDate: '2025-04-20',
+      isCompleted: false,
+      createdAt: 1710000000000,
+      updatedAt: 1710000000000
+    },
+    {
+      id: 2,
+      name: 'Finish React Project',
+      description: 'Implement CRUD features and localStorage',
+      dueDate: '2025-04-22',
+      isCompleted: false,
+      createdAt: 1710000500000,
+      updatedAt: 1710000500000
+    },
+    {
+      id: 3,
+      name: 'Workout Session',
+      description: 'Cardio and strength training at the gym',
+      dueDate: '2025-04-21',
+      isCompleted: false,
+      createdAt: 1710001000000,
+      updatedAt: 1710001200000
+    },
+    {
+      id: 4,
+      name: 'Call Doctor',
+      description: 'Schedule appointment for annual check-up',
+      dueDate: '2025-04-19',
+      isCompleted: false,
+      createdAt: 1710001500000,
+      updatedAt: 1710001500000
+    },
+    {
+      id: 5,
+      name: 'Read a book',
+      description: 'Finish reading Chapter 5 of “Atomic Habits”',
+      dueDate: '2025-04-25',
+      isCompleted: false,
+      createdAt: 1710002000000,
+      updatedAt: 1710002000000
+    }
+  ];
