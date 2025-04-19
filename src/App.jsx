ec75d03a74e12eb53b0d453f58404688491a8126
Ark-Arch <69996990+Ark-Arch@users.noreply.github.com>
2025-04-19 06:23:38 +0000

    included a route in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import TaskForm from './components/TaskForm/TaskForm'
 import TaskList from './components/TaskList/TaskList'
+import { Routes, Route } from 'react-router-dom'
 
 import { tasks } from './assets/taskData'
 
 function App() {
     return (
         <>
-            <TaskList tasks={tasks}/>
+            <Routes>
+                <Route path='/task-list' element={<TaskList tasks={tasks}/>}/>
+                <Route path='/add-task' element={<TaskForm/>}/>
+            </Routes>
         </>
     )
 }
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TaskOneLiner from './TaskOneLiner'
+import { Link } from 'react-router-dom'
 
 
 const TaskList = ({tasks}) => {
@@ -8,7 +9,7 @@ const TaskList = ({tasks}) => {
             <div className="container my-5">
             <div className="d-flex justify-content-between align-items-center mb-4">
                 <h3 className="mb-0">My To-Do List</h3>
-                <button className="btn btn-primary px-4">Add</button>
+                <Link to='/add-task'><button className="btn btn-primary px-4">Add</button></Link>
             </div>
             
             <div className="list-group">
